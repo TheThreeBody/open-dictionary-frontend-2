040327fb023a5b2975c5f605a9cff2af438bbe53
Chan Woo Kim <chanwookim@Chans-MacBook-Pro.local>
2017-11-25 19:44:18 +0800

    home page change

--- src/routes.js
+++ src/routes.js
@@ -10,8 +10,7 @@ import Addword from './pages/addword/AddwordContainer';
 
 export default (
     <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="#">Open Dictionary</a>
+        <nav class="navbar navbar-expand-lg navbar-design">
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
