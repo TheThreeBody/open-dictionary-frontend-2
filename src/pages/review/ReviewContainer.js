5098ab76b48b465227145df93676d276e9aa1ce9
Chan Woo Kim <kim.chanwoo@yahoo.com>
2017-11-25 20:46:04 +0800

    changed review

--- src/pages/review/ReviewContainer.js
+++ src/pages/review/ReviewContainer.js
@@ -15,10 +15,72 @@ export default class LoginContainer extends Component {
     render() {
         return (
             <div>
-                <p>Word</p>
-                <p>Translation</p>
-                <button>Approve</button>
-                <button>Reject</button>
+              <div class="contain">
+                <h1 class="main-label">Here are some words for you to review:</h1>
+                <h3 class="main-sub-label">Quality assurances like these sincerely help sustain the reliability of our dictionary.</h3>
+                <ul class="ul-contain-review-words">
+                  <li class="review-words-item">
+                    <ul class="within-item">
+                      <li>
+                        <div class="contain-items-in-list contain-word">
+                          <p class="text-within-item">Word: <b class="actual-word">你好</b></p>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="contain-items-in-list contain-translation">
+                          <p class="text-within-item">Translation: <b class="actual-word">Hello</b></p>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="contain-items-in-list contain-judgement">
+                          <button class="judgement-buttons judge-approve-btn">Approve</button>
+                          <button class="judgement-buttons judge-rejected-btn">Reject</button>
+                        </div>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="review-words-item">
+                    <ul class="within-item">
+                      <li>
+                        <div class="contain-items-in-list contain-word">
+                          <p class="text-within-item">Word: <b class="actual-word">早上好</b></p>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="contain-items-in-list contain-translation">
+                          <p class="text-within-item">Translation: <b class="actual-word">Good morning</b></p>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="contain-items-in-list contain-judgement">
+                          <button class="judgement-buttons judge-approve-btn">Approve</button>
+                          <button class="judgement-buttons judge-rejected-btn">Reject</button>
+                        </div>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="review-words-item">
+                    <ul class="within-item">
+                      <li>
+                        <div class="contain-items-in-list contain-word">
+                          <p class="text-within-item">Word: <b class="actual-word">讨厌</b></p>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="contain-items-in-list contain-translation">
+                          <p class="text-within-item">Translation: <b class="actual-word">Love</b></p>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="contain-items-in-list contain-judgement">
+                          <button class="judgement-buttons judge-approve-btn">Approve</button>
+                          <button class="judgement-buttons judge-rejected-btn">Reject</button>
+                        </div>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
             </div>
         )
     }
