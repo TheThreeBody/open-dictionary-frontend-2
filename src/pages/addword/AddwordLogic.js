926644cd71946d57a571eb87fbe5e3f43d3918a0
han4wluc <han4wluc@gmail.com>
2017-11-25 19:43:24 +0800

    add word

--- src/pages/addword/AddwordLogic.js
+++ src/pages/addword/AddwordLogic.js
@@ -2,14 +2,42 @@ import {createLogic} from 'redux-logic';
 import axios from 'axios';
 
 const homeLogic = createLogic({
-    type: 'ADDWORD_DATA_REQUEST', // only apply this logic to this type
-    cancelType: 'CANCEL_FETCH_POLLS', // cancel on this type
+    type: 'ADDWORD_TRANSLATION', // only apply this logic to this type
     latest: true, // only take latest
-    process({
+    async process({
         getState,
         action
     }, dispatch, done) {
-        
+      const { hanzi, pinyin, translation } = action.payload;
+      console.warn(action.payload)
+
+const query = `
+mutation($hanzi: String, $pinyin: String, $translation: String) {
+  addWord (input: {
+    hanzi: $hanzi,
+    pinyin: $pinyin,
+    translation: $translation,
+    userId: 3
+  }) {
+    clientMutationId
+  }
+}
+`
+      try {
+        const result = await axios.post('http://106.14.4.255:3000/graphql', {
+          query,
+          variables: {
+            hanzi,
+            pinyin,
+            translation
+          }
+        })
+        console.log(result);
+      } catch (error){
+        console.warn(error);
+      }
+
+
 
     }
 });
