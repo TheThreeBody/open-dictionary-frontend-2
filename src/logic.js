05f2f6da590c554a50d0c183ee29a44ea977e14c
han4wluc <han4wluc@gmail.com>
2017-11-25 20:39:43 +0800

    review

--- src/logic.js
+++ src/logic.js
@@ -1,9 +1,11 @@
 import homeLogic from './pages/home/HomeLogic';
 import loginLogic from './pages/login/LoginLogic';
 import addwordLogic from './pages/addword/AddwordLogic';
+import reviewLogic from './pages/review/ReviewLogic';
 
 export default[
     ...homeLogic,
     ...loginLogic,
     ...addwordLogic,
+    ...reviewLogic,
 ];
--- src/pages/review/ReviewLogic.js
+++ src/pages/review/ReviewLogic.js
@@ -1,4 +1,97 @@
 import {createLogic} from 'redux-logic';
 import axios from 'axios';
 
-export default[];
+const reviewWord = createLogic({
+    type: 'REVIEW_TRANSLATION', // only apply this logic to this type
+    latest: true, // only take latest
+    async process({
+        getState,
+        action
+    }, dispatch, done) {
+
+const query = `
+query {
+  allTranslations(first: 100) {
+    nodes {
+      english,
+      hanzi,
+      pinyin
+    }
+  }
+}
+`
+      try {
+        const result = await axios.post('http://106.14.4.255:3000/graphql', {
+          query
+        })
+        console.log(result.data.data.allTranslations.nodes);
+      } catch (error){
+        console.warn(error);
+      }
+    }
+});
+
+
+const reviewWord = createLogic({
+    type: 'REVIEW_REJECT', // only apply this logic to this type
+    latest: true, // only take latest
+    async process({
+        getState,
+        action
+    }, dispatch, done) {
+      const { translationId } = action.payload;
+    const query = `
+    mutation {
+      reviewTranslation (input: {
+        translationId: ${translationId},
+        status: -1
+      }) {
+        clientMutationId
+      }
+    }
+    `
+      try {
+        const result = await axios.post('http://106.14.4.255:3000/graphql', {
+          query
+        })
+        console.warn('reject success')
+        // console.log(result.data.data.allTranslations.nodes);
+      } catch (error){
+        console.warn(error);
+      }
+    }
+});
+
+const reviewWord = createLogic({
+    type: 'REVIEW_APPROVE', // only apply this logic to this type
+    latest: true, // only take latest
+    async process({
+        getState,
+        action
+    }, dispatch, done) {
+      const { translationId } = action.payload;
+    const query = `
+    mutation {
+      reviewTranslation (input: {
+        translationId: ${translationId},
+        status: 2
+      }) {
+        clientMutationId
+      }
+    }
+    `
+      try {
+        const result = await axios.post('http://106.14.4.255:3000/graphql', {
+          query
+        })
+        console.warn('approve success')
+        // console.log(result.data.data.allTranslations.nodes);
+      } catch (error){
+        console.warn(error);
+      }
+    }
+});
+
+
+export default[reviewWord];
+
